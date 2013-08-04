08556faff7c054a544e0143bb731c81b8028d8de
Soumya Deb <debloper@gmail.com>
2013-08-04 18:48:45 +0530

    Reads and separates property & rules from the given MASS

--- reactor.pl
+++ reactor.pl
@@ -1,8 +1,22 @@
 #!/usr/bin/perl
 
+# Open the file
+# Read the file content as strings
+# Store the string for further processing
+# Close the file
+# TODO: read the filename from arguments
+$MASS = "";
 open (MASS, 'sample.mass');
-while (<MASS>) {
-	chomp;
-	print "$_\n";
-}
+local $/;
+$MASS = <MASS>;
 close (MASS);
+
+# Get the property names in order
+@prop = $MASS =~ /\n(.+){/gm;
+print join(", ", @prop);
+
+print "\n\n";
+
+# Get the rulesets for the properties in order
+@rules = $MASS =~ /{\n(.+?)\n}/gsm;
+print join("\n==========\n", @rules)."\n";
